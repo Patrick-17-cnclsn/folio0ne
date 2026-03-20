8dbcd2e5fa9461cbadd965168d326768310c36b1
hassa <offoumoupatrick0@gmail.com>
2026-03-20 15:23:24 +0100

    Add `CONFIG` object, integrate `input-otp` components, and include ShadcnHelper settings file.

---
+++ lib/config.ts
@@ -0,0 +1,34 @@
+export const CONFIG = {
+  name: "Offoumou",
+  role: "Développeur Fullstack",
+  location: "France",
+  email: "offoumoupatrick0@gmail.com",
+  phone: "+33 6 00 00 00 00",
+  bioShort:
+    "Développeur Fullstack passionné par la création d'expériences utilisateur fluides et de solutions techniques robustes.",
+  bioLong:
+    "Je suis Patrick Offoumou, un développeur Fullstack junior  basé en France. Passionné par le développement web, je me spécialise dans la création d'applications modernes en utilisant les dernières technologies.",
+  socials: {
+    twitter: "https://twitter.com/hassan",
+    github: "https://github.com/Patrick-17-cnclsn",
+    linkedin: "",
+    dribbble: "https://dribbble.com/hassan"
+  },
+  services: [
+    {
+      title: "Design de Produit",
+      price: "20€",
+      icon: "PaletteIcon"
+    },
+    {
+      title: "Développement Frontend",
+      price: "50€",
+      icon: "FolderCodeIcon"
+    },
+    {
+      title: "Branding",
+      price: "55€",
+      icon: "Building2Icon"
+    }
+  ]
+};
